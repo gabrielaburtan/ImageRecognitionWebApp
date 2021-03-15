ea7b8d88991df527924025e28b877fbe5501eef5
Burtan Gabriela <burtangabriela@gmail.com>
2021-03-15 19:06:27 +0200

    Updated header component

---
+++ fsd-app/src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HeaderStateService } from 'src/app/services/header-state.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  isLoggedIn$: Observable<boolean> ;
+
+  constructor(private headerService: HeaderStateService) {
+    this.isLoggedIn$ = of(false);
+   }
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.headerService.isLoggedIn;
+  }
+
+  onLogout(){
+    
+  }
+
+}
